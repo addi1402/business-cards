c910f5ed180596d04c8a153cf3fe4f2843177e9c
Aditya Patil <aditya142002@gmail.com>
2023-06-21 01:48:00 +0530

    Add Icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "../stylesheets/Footer.css";
+import Facebook from "../assets/facebook.svg";
+import Twitter from "../assets/twitter.svg";
+import GitHub from "../assets/github.svg";
+import Inbox from "../assets/inbox.svg";
+import LinkedIn from "../assets/linkedin.svg";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <img className="social-handle" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
-      <img className="social-handle" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
-      <img className="social-handle" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
-      <img className="social-handle" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />
+      <img className="social-handle" src={Facebook} />
+      <img className="social-handle" src={Twitter} />
+      <img className="social-handle" src={LinkedIn} />
+      <img className="social-handle" src={GitHub} />
+      <img className="social-handle" src={Inbox} />
     </footer>
   );
 }
